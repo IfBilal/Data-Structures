ed820e62f1398532d82faf50cebd835feb275de9
IfBilal <muhammadbilaltahir06@gmail.com>
2025-08-25 01:29:03 +0500

    linked list with tail ptr

---
+++ LinkedList/LinkedListWithTail.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        else
+        {
+            Node *temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+        }
+    }
+    void pop_back()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next != tail)
+            {
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            delete tail;
+            tail = temp;
+        }
+    }
+    void removeByIndex(int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+    }
+    void printLinkedList()
+    {
+        for (Node *temp = head; temp != NULL; temp = temp->next)
+        {
+            cout << temp->data << "  ";
+        }
+        cout << endl;
+    }
+    
+};
+int main()
+{
+    LinkedList List;
+    List.push_front(5);
+    List.push_front(6);
+    List.push_back(1);
+    List.push_back(2);
+    List.printLinkedList();
+    List.pop_front();
+    List.printLinkedList();
+    List.pop_back();
+    List.printLinkedList();
+}
