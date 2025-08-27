dd5f88c453c9157ba19410da47b0597801a0a1a9
IfBilal <muhammadbilaltahir06@gmail.com>
2025-08-27 19:51:52 +0500

    middle of linked list

---
+++ LinkedList/middleOfLinkedList.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    void printLinkedList()
+    {
+        for (Node *temp = head; temp != NULL; temp = temp->next)
+        {
+            cout << temp->data << "  ";
+        }
+        cout << endl;
+    }
+    void middleOfLinkedList()
+    {
+        Node *temp = head;
+        Node *middle = head;
+        while (temp != NULL && temp->next != NULL)
+        {
+            temp = temp->next->next;
+            middle = middle->next;
+        }
+        cout<<"The data of middle node is : "<<middle->data<<endl;
+    }
+};
+int main()
+{
+    LinkedList list;
+    list.push_back(5);
+    list.push_back(4);
+    list.push_back(3);
+    list.push_back(2);
+    list.push_back(1);
+    list.push_back(0);
+    list.printLinkedList();
+    list.middleOfLinkedList();
+}
